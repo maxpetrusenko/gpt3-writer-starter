5efe6a871a10335496f220c94c4ea738f67c1d16
maxpetrusenko <max.petrusenko@gmail.com>
2022-11-28 10:40:28 -0500

    removed api key

---
+++ pages/api/generate.js
@@ -0,0 +1,31 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const basePromptPrefix =
+`
+Generate a tweet. Make it funny. Add a meme
+
+Title:
+`;
+const generateAction = async (req, res) => {
+  // Run first prompt
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-002',
+    prompt: `${basePromptPrefix}${req.body.userInput}\n`,
+    temperature: 0.9,
+    max_tokens: 280,
+  });
+  
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  res.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction;
